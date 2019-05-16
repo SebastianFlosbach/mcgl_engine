0b87a48009635e8cd3e08875aaf41b4128de62b2
SebastianFlosbach <sebflos@online.de>
2019-05-16 12:40:53 +0200

    Better logging

--- engine/source/src/mcgl-engine.cpp
+++ engine/source/src/mcgl-engine.cpp
@@ -15,14 +15,6 @@ std::unique_ptr<ConsoleLogger> logger;
 std::unique_ptr<Engine> engine;
 
 
-bool checkEngine() {
-	if( !engine ) {
-		return false;
-	}
-
-	return true;
-}
-
 void CreateEngine() {
 	logger = std::make_unique<ConsoleLogger>( /*loggerName, loggerPath*/ );
 	info( *logger, "[MCGL-ENGINE] CreateEngine" );
@@ -41,7 +33,10 @@ void DestroyEngine() {
 }
 
 void CreateWindow( NUM32 width, NUM32 height, const std::string& title ) {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] CreateEngine: Engine not created!" );
+		return;
+	}
 
 	info( *logger, "[MCGL-ENGINE] CreateWindow" );
 	
@@ -49,115 +44,164 @@ void CreateWindow( NUM32 width, NUM32 height, const std::string& title ) {
 }
 
 void CloseWindow() {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] CloseWindow: Engine not created!" );
+		return;
+	}
 
 	info(*logger, "[MCGL-ENGINE] CloseWindow");
 	engine->closeWindow();
 }
 
 void Run() {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] Run: Engine not created!" );
+		return;
+	}
 
 	trace( *logger, "[MCGL-ENGINE] Run" );
 	engine->run();
 }
 
 void Stop() {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] Stop: Engine not created!" );
+		return;
+	}
 
-	trace( *logger, "[MCGL-ENGINE] Stop" );
+	info( *logger, "[MCGL-ENGINE] Stop" );
 	engine->stop();
 }
 
 void RegisterBlockType( const chunk::block::Block& block ) {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] RegisterBlockType: Engine not created!" );
+		return;
+	}
 
-	trace( *logger, "[MCGL-ENGINE] RegisterBockType" );
+	info( *logger, "[MCGL-ENGINE] RegisterBockType" );
 	engine->registerBlockType( block );
 }
 
 void SetTextures( const std::string& path, UNUM32 textureSize, UNUM32 textureCount ) {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] SetTextures: Engine not created!" );
+		return;
+	}
 
-	trace(*logger, "[MCGL-ENGINE] SetTextures");
+	info(*logger, "[MCGL-ENGINE] SetTextures");
 
 	engine->setTextures( path, textureSize, textureCount );
 }
 
 void SetShader( const std::string& vertexShaderPath, const std::string& fragmentShaderPath ) {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] SetShader: Engine not created!" );
+		return;
+	}
 
-	trace(*logger, "[MCGL-ENGINE] SetShader");
+	info(*logger, "[MCGL-ENGINE] SetShader");
 
 	engine->setShader( vertexShaderPath, fragmentShaderPath );
 }
 
 void AddChunk( const chunk::Chunk& chunk ) {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] AddChunk: Engine not created!" );
+		return;
+	}
 
-	trace( *logger, "[MCGL-ENGINE] AddChunk" );
+	debug( *logger, "[MCGL-ENGINE] AddChunk" );
 	engine->addChunk( chunk );
 }
 
 void RemoveChunk( NUM32 x, NUM32 z) {
-	if (!checkEngine()) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] RemoveChunk: Engine not created!" );
+		return;
+	}
 
 	trace(*logger, "[MCGL-ENGINE] RemoveChunk");
 	engine->removeChunk( x, z );
 }
 
 void CreateCamera( float x, float y, float z, float pitch, float yaw, float roll ) {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] CreateCamera: Engine not created!" );
+		return;
+	}
 
-	trace( *logger, "[MCGL-ENGINE] CreateCamera" );
+	info( *logger, "[MCGL-ENGINE] CreateCamera" );
 	
 	engine->createCamera( x, y, z, pitch, yaw, roll );
 }
 
 void MoveCamera( UNUM32 cameraId, float dx, float dy, float dz ) {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] MoveCamera: Engine not created!" );
+		return;
+	}
 
 	trace( *logger, "[MCGL-ENGINE] MoveCamera" );
 	engine->moveCamera( cameraId, dx, dy, dz );
 }
 
 void RotateCamera( UNUM32 cameraId, float pitch, float yaw, float roll ) {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] RotateCamera: Engine not created!" );
+		return;
+	}
 
 	trace( *logger, "[MCGL-ENGINE] RotateCamera" );
 	engine->rotateCamera( cameraId, pitch, yaw, roll );
 }
 
 void RegisterKeyEventCallback( MCGL_KEY_EVENT_CALLBACK callback ) {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] RegisterKeyEventCallback: Engine not created!" );
+		return;
+	}
 
-	trace( *logger, "[MCGL-ENGINE] RegisterKeyEventCallback" );
+	info( *logger, "[MCGL-ENGINE] RegisterKeyEventCallback" );
 	engine->registerKeyEventCallback( callback );
 }
 
 void RegisterMouseEventCallback( MCGL_MOUSE_EVENT_CALLBACK callback ) {
-	if ( !checkEngine() ) return;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] RegisterMouseEventCallback: Engine not created!" );
+		return;
+	}
 
-	trace( *logger, "[MCGL-ENGINE] RegisterMouseEventCallback" );
+	info( *logger, "[MCGL-ENGINE] RegisterMouseEventCallback" );
 	engine->registerMouseEventCallback( callback );
 }
 
 void RegisterStatusEventCallback( MCGL_STATUS_EVENT_CALLBACK callback ) {
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] RegisterStatusEventCallback: Engine not created!" );
+		return;
+	}
+	
+	info( *logger, "[MCGL-ENGINE] RegisterStatusEventCallback" );
 	engine->registerStatusEventCallback( callback );
 }
 
-void Set( MCGL_CONFIG config, void* data ) {
-	throw std::runtime_error( "Not implemented" );
-}
-
 float GetDeltaTime() {
-	if ( !checkEngine() ) return -1.0f;
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] GetDeltaTime: Engine not created!" );
+		return -1.f;
+	}
 
+	trace( *logger, "[MCGL-ENGINE] GetDeltaTime" );
 	return engine->getDeltaTime();
 }
 
 coordinates::WorldCoordinates GetCameraPosition( UNUM32 cameraId ) {
-	if( !checkEngine() ) return { 0, 0, 0 };
+	if( !engine ) {
+		error( *logger, "[MCGL-ENGINE] GetCameraPosition: Engine not created!" );
+		return { 0, 0, 0 };
+	}
 
+	trace( *logger, "[MCGL-ENGINE] GetCameraPosition" );
 	return engine->getCameraPosition( cameraId );
 }
